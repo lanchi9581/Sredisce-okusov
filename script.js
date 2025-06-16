23a6569c1b8bff5f98c0a5e3324c728bbca4157c
lanchi9581 <lan.zvajker@gmail.com>
2025-06-16 14:19:57 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function prikaziSporocilo() {
+  alert("Klikni sosednji gumb za spremenitev ozadja");
+}
+
+function spremeniBarvoOzadja() {
+  document.body.style.backgroundImage = "url('Background2-inverted.png')";
+  document.body.style.backgroundRepeat = "repeat";
+  document.body.style.backgroundPosition = "center";
+}
